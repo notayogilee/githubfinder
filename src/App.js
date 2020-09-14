361c24a1dea5dcfc051bbe5ca5c2b72df99caa03
Lee Castelani <notayogilee@gmail.com>
2020-09-14 16:05:20 -0400

    changed state to useContext

--- src/App.js
+++ src/App.js
@@ -1,101 +1,46 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import './App.css';
-import axios from 'axios';
+import GitHubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
 
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-  // Search github users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
-  // Get a single github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}?access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
-
-    setUser(res.data);
-    setLoading(false);
-  }
-
-  // Get users repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
-
-    setRepos(res.data);
-    setLoading(false);
-  }
-
-  // Clear Users from state
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
-
-  // Set alert 
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 5000)
-  }
-
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search
-                  searchUsers={searchUsers}
-                  clearUsers={clearUsers}
-                  showClear={users.length > 0 ? true : false}
-                  setAlert={showAlert}
+    <GitHubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route
+                  exact path='/'
+                  component={Home}
                 />
-                <Users loading={loading} users={users} />
-              </Fragment>
-            )} />
-            <Route exact path='/about' component={About} />
-            <Route
-              exact path='/user/:login'
-              render={props => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
+                <Route
+                  exact path='/about'
+                  component={About}
                 />
-              )} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+                <Route
+                  exact path='/user/:login'
+                  component={User}
+                />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GitHubState>
   );
-}
+};
 
 export default App;
---
+++ src/context/github/GithubState.js
@@ -0,0 +1,84 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS
+} from '../types';
+
+const GitHubState = props => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false
+  }
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Search Users
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    })
+  };
+
+  // Get User
+  const getUser = async (username) => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/users/${username}?access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
+
+
+
+  // Get Repos
+  const getUserRepos = async (username) => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&access_token=${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    })
+  };
+
+  // Clear Users
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  return (<GithubContext.Provider
+    value={{
+      users: state.users,
+      user: state.user,
+      repos: state.repos,
+      loading: state.loading,
+      searchUsers,
+      clearUsers,
+      getUser,
+      getUserRepos
+    }}
+  >
+    {props.children}
+  </GithubContext.Provider>
+  );
+};
+
+export default GitHubState;
